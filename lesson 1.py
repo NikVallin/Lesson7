37dff18d72e78ea712b912db6b84519bde1bcdf0
NikVallin <busina@mail.ru>
2023-03-15 19:50:35 +0300

    chisla

---
+++ lesson 1.py
@@ -0,0 +1,17 @@
+
+print(1**100)
+print(2**16)
+print(2-1000)
+print(2+2*2)
+print(60/3)
+import math
+print(math.sqrt(144))
+x1 =2+5*14
+print(x1)
+x2 =66-11
+print(x2)
+x3 =16-8
+print(math.sqrt(x3))
+
+
+
